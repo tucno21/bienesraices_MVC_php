7e6a870f3726ee51cbc7f79405240a522bbd0e90
tucno21 <carlitostucno@gmail.com>
2021-08-10 12:08:14 -0500

    captura de rutas get y su funcion

--- Router.php
+++ Router.php
@@ -4,11 +4,38 @@ namespace MVC;
 
 class Router
 {
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    //recibe las  rutas parametros de PUBLIC/INDEX
+    public function get($url, $fn)
+    {
+        //almacena en rutasGET
+        $this->rutasGET[$url] = $fn;
+    }
+
     public function comprobarRutas()
     {
         //captura el nombre de la url
         $urlActual = $_SERVER["PATH_INFO"] ?? '/';
 
-        debuguear($urlActual);
+        //capturar el tipo de ruta get o post
+        $metodo = $_SERVER["REQUEST_METHOD"];
+
+
+        if ($metodo == "GET") {
+            //almacenar la funcion asociada a la url de PUBLIC/INDEX
+            $fn =  $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if ($fn) {
+            //si la url existe con la funcion asociada
+        } else {
+            //o manadar a la pagina 404
+            echo "Pagina no existe";
+        }
+
+        debuguear("");
     }
 }
--- public/index.php
+++ public/index.php
@@ -6,5 +6,11 @@ use MVC\Router;
 
 $router = new Router();
 
+//le pasamos la url y la funcion al ROUTER
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/tienda_virtual', 'funcion_tienda');
+$router->get('/', 'funcion_admin');
+
+
 //lamando el metodo de ruter
 $router->comprobarRutas();
