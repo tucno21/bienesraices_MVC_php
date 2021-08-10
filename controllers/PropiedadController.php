fc02873775cba9aa6cffcedbd1ad1fa21053ab3c
tucno21 <carlitostucno@gmail.com>
2021-08-10 15:30:07 -0500

    llamado al modelo dentro de controlador BD

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -3,15 +3,23 @@
 namespace Controllers;
 //llamando al router para llamar a las vistas desde el controlador
 use MVC\Router;
+//llamar al modelo para obtener la base de datos
+use Model\Propiedad;
+use Model\Vendedor;
 
 class PropiedadController
 {
     //pasamos la url y la funcion del router index
     public static function index(Router $router)
     {
+        //traer los datos de todas las propiedades
+        $propiedades = Propiedad::all();
+        $vendedores = Vendedor::all();
+
+
         $router->render('propiedades/admin', [
-            'mensaje' => 'mensaje2',
-            'propiedades' => 'casa'
+            'propiedades' => $propiedades,
+            'vendedores' => $vendedores,
         ]);
     }
 
--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -1,4 +1,79 @@
-<?php
+<main class="contenedor seccion">
+    <h1>Administrador de bienes raices</h1>
 
-echo $mensaje;
-echo $propiedades;
+    <?php
+    $mensaje = muestraNotificacion(intval($resultado));
+    if ($mensaje) :
+    ?>
+        <p class="alerta exito"><?php echo s($mensaje); ?></p>
+    <?php endif; ?>
+
+    <a class="boton boton-verde" href="/admin/propiedades/crear.php">Nueva Propiedad</a>
+    <a class="boton boton-amarillo" href="/admin/vendedores/crear.php">Nuevo(a) Vendedor(a)</a>
+
+    <h2>Propiedades</h2>
+    <table class="propiedad">
+        <thead class="verder">
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <thead>
+            <!-- //generar a partir de la base de datos -->
+            <?php foreach ($propiedades as $propiedad) : ?>
+                <tr>
+                    <th><?php echo $propiedad->id; ?></th>
+                    <th><?php echo $propiedad->titulo; ?></th>
+                    <th><img src="/imagenes/<?php echo $propiedad->imagen; ?>" class="imagen-tabla" alt="casa"></th>
+                    <th>$ <?php echo $propiedad->precio; ?></th>
+                    <th>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+                            <input type="hidden" name="tipo" value="propiedad">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a class="boton-amarillo-block" href="admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>">Actualizar</a>
+                    </th>
+                </tr>
+            <?php endforeach; ?>
+        </thead>
+    </table>
+
+    <h2>Vendedores</h2>
+    <table class="propiedad">
+        <thead class="verder">
+            <tr>
+                <th>ID</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Telefono</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <thead>
+            <!-- //generar a partir de la base de datos -->
+            <?php foreach ($vendedores as $vendedor) : ?>
+                <tr>
+                    <th><?php echo $vendedor->id; ?></th>
+                    <th><?php echo $vendedor->nombre; ?></th>
+                    <th><?php echo $vendedor->apellidos; ?></th>
+                    <th><?php echo $vendedor->telefono; ?></th>
+                    <th>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $vendedor->id; ?>">
+                            <input type="hidden" name="tipo" value="vendedor">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a class="boton-amarillo-block" href="admin/vendedores/actualizar.php?id=<?php echo $vendedor->id; ?>">Actualizar</a>
+                    </th>
+                </tr>
+            <?php endforeach; ?>
+        </thead>
+    </table>
+</main>
