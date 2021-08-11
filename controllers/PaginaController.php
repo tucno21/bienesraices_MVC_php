b1c5a674462bfbd0fb23be15b7cbabcedb25a0fa
tucno21 <carlitostucno@gmail.com>
2021-08-11 12:32:35 -0500

    add pphmailer

--- controllers/PaginaController.php
+++ controllers/PaginaController.php
@@ -53,8 +53,71 @@ class PaginaController
         $router->render('/paginas/entrada', []);
     }
 
-    public static function contacto()
+    public static function contacto(Router $router)
     {
-        echo "desde el contacto";
+        $mensaje = null;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+            // debuguear($_POST);
+            //capturando datos del post
+            $respuesta = $_POST['contacto'];
+
+            //crear una isntancia de PHPmailer
+            $mail = new PHPMailer();
+
+            //configurar SMTP protocolo para envio de email
+            $mail->isSMTP();
+            $mail->Host = "smtp.mailtrap.io"; //viene del servidor
+            $mail->SMTPAuth = true;
+            $mail->Username = "cd8cbe192deca7";
+            $mail->Password = "62fd2afb84c11f";
+            $mail->SMTPSecure = "tls";
+            $mail->Port  = "2525";
+
+            //configurar el contenido del email
+            $mail->setFrom('carlitostucno@gmail.com');
+            $mail->addAddress('carlitostucno@gmail.com', 'catuva.com');
+            $mail->Subject = 'Nuevo mensaje WEB';
+
+            //habilitar HTML
+            $mail->isHTML(true);
+            $mail->charset = 'UTF-8';
+
+            //denifinir el contenido del
+            $contenido = '<html>';
+            $contenido .= '<p>Tienes un nuevo mensaje</p>';
+            $contenido .= '<p>Nombre: ' . $respuesta['nombre'] . '</p>';
+
+            if ($respuesta['contacto'] === 'telefono') {
+                $contenido .= '<p> Eligio ser contactado por telefono</p>';
+                $contenido .= '<p>Telefono: ' . $respuesta['telefono'] . '</p>';
+                $contenido .= '<p>Fecha Contacto: ' . $respuesta['fecha'] . '</p>';
+                $contenido .= '<p>Hora Contacto: ' . $respuesta['hora'] . '</p>';
+            } else {
+                //es email est
+                $contenido .= '<p> Eligio ser contactado por Email</p>';
+                $contenido .= '<p>Email: ' . $respuesta['email'] . '</p>';
+            }
+
+            $contenido .= '<p>Mensaje: ' . $respuesta['mensaje'] . '</p>';
+            $contenido .= '<p>Vende o Compra: ' . $respuesta['tipo'] . '</p>';
+            $contenido .= '<p>Presupuesto: $' . $respuesta['precio'] . '</p>';
+            // $contenido .= '<p>Prefiere ser contactado por: ' . $respuesta['contacto'] . '</p>';
+            $contenido .= '</html>';
+
+            $mail->Body = $contenido;
+            $mail->AltBody = 'este es texto alternativo sin HTML';
+
+            //envia el mail
+            if ($mail->send()) {
+                $mensaje = 'mensaje enviado';
+            } else {
+                $mensaje = 'el mensaje no se pudo enviar';
+            }
+        }
+        $router->render('/paginas/contacto', [
+            'mensaje' => $mensaje
+        ]);
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -8,13 +8,36 @@ document.addEventListener("DOMContentLoaded", function(){
 function eventListener() {
     const mobilMmenu = document.querySelector(".mobile-menu");
     mobilMmenu.addEventListener('click', navegacionResposive);
+
+    //muestra campos condicionales
+    const metodoContacto = document.querySelectorAll('input[name="contacto[contacto]"]');
+    metodoContacto.forEach(input => input.addEventListener('click', mostrarMetodoContacto));
 }
 
 function navegacionResposive(){
     const navegacion = document.querySelector(".navegacion");
-    
-
     navegacion.classList.toggle('mostrar');
+}
+
+function mostrarMetodoContacto(e){
+const contactoDiv = document.querySelector("#contacto");
+if(e.target.value === 'telefono'){
+    contactoDiv.innerHTML = `
+    <label for="telefono">Numero Teléfono </label>
+    <input type="tel" placeholder="Tu Teléfono" id="telefono" name="contacto[telefono]" required>
+    <p>Elija la fecha y la hora Para la llamada</p>
+    <label for="fecha">Fecha:</label>
+    <input type="date" id="fecha" name="contacto[fecha]">
+    <label for="hora">Hora:</label>
+    <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
+    `;
+
+}else{
+    contactoDiv.innerHTML =`
+    <label for="email">E-mail</label>
+    <input type="email" placeholder="Tu Email" id="email" name="contacto[email]" required>
+    `;
+}
 }
 
 function  darkMode(){
